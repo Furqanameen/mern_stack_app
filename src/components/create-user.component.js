b9ff47a7865bb02926b03fd1c004826b8faaf93e
furqanameen <furqanbinameen@gmail.com>
2024-01-06 01:04:18 +0400

    Front end designing

---
+++ src/components/create-user.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class CreateUser extends Component {
+  render() {
+    debugger
+    console.log("this is");
+    return (
+      <div>
+        debugger
+        console.log("component rendered")
+        <p>You are on the Create User component!</p>
+      </div>
+    )
+  }
+}
---
+++ src/user.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+function User() {
+  return (
+    <div className="User">
+     <h1>hello this is render page of user to get details from user etc</h1>
+    </div>
+  );
+}
+
+export default User;
